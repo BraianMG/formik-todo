9f2e51a31d377c7fc303f89c43f1fed37a3ebd9e
Braian <braian.gonzales77@gmail.com>
2022-05-13 20:40:56 -0300

    arreglar errores generados por la actualización de la rama

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -2,10 +2,10 @@ import { useContext } from "react";
 import AddTodoBar from "./AddTodoBar";
 import TodoItem from "./TodoItem";
 import { TodosContext } from "../context/todos";
+import { Todo } from "../interfaces";
 
 const Todos: React.FC = () => {
-  const { todos, addTodo, toggleTodoStatus, removeTodo } =
-    useContext(TodosContext);
+  const { todos, addTodo, updateTodo, removeTodo } = useContext(TodosContext);
 
   return (
     <div className="w-full sm:w-9/12 lg:w-7/12 mx-auto my-20">
@@ -19,12 +19,9 @@ const Todos: React.FC = () => {
         return (
           <TodoItem
             key={todo.id}
-            id={todo.id}
-            title={todo.title}
-            description={todo.description}
-            completed={todo.completed}
-            toggleTodoStatus={(id: string) => toggleTodoStatus({ id })}
-            removeTodo={(id: string) => removeTodo({ id })}
+            todo={todo}
+            updateTodo={(todo: Todo) => updateTodo(todo)}
+            removeTodo={(id: string) => removeTodo(id)}
           />
         );
       })}
